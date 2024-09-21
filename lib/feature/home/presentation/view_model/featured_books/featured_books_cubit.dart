048b25d58ff6687343f1ae6c0ffb9c8d81dd70a1
mohamedamr77 <moamr947@gmail.com>
2024-09-21 18:36:45 +0300

    done  FeaturedBooksCubit and FeaturedBooksState

---
+++ lib/feature/home/presentation/view_model/featured_books/featured_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/feature/home/data/model/book_model.dart';
+import 'package:booklyapp/feature/home/data/repo/home_repo.dart';
+import 'package:booklyapp/feature/home/presentation/view_model/featured_books/featured_books_state.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../../core/error/faliure.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitialState());
+   List<BookModel> featuredBooksList=[];
+   late  HomeRepo homeRepo;
+  fetchFeaturedBooks()async{
+    emit(FeaturedBooksLoadingState());
+    Either<Failure, List<BookModel>> result =await homeRepo.fetchFeaturedBooks();
+    result.fold(
+            (faliure){
+              emit(FeaturedBooksFaliureState(errorMessage: faliure.errorMessage));
+            }, (success){
+              featuredBooksList=success;
+            emit(FeaturedBooksSuccessState());
+    });
+  }
+
+}
---
+++ lib/feature/home/presentation/view_model/featured_books/featured_books_state.dart
@@ -0,0 +1,8 @@
+abstract class FeaturedBooksState{}
+class FeaturedBooksInitialState extends FeaturedBooksState{}
+class FeaturedBooksLoadingState extends FeaturedBooksState{}
+class FeaturedBooksFaliureState extends FeaturedBooksState{
+  final String errorMessage;
+  FeaturedBooksFaliureState({required this.errorMessage});
+}
+class FeaturedBooksSuccessState extends FeaturedBooksState{}
