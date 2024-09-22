b6f56e665ea4d01dbf908e28865e912eda933c0e
mohamedamr77 <moamr947@gmail.com>
2024-09-22 21:03:01 +0300

    make loading state in newest

--- lib/feature/home/presentation/view/widgets/newest_list_view.dart
+++ lib/feature/home/presentation/view/widgets/newest_list_view.dart
@@ -19,26 +19,115 @@ class BestSellerListView extends StatefulWidget {
 class _BestSellerListViewState extends State<BestSellerListView> {
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     BlocProvider.of<NewestBooksCubit>(context).fetchNewestBooks();
-}
+  }
 
-@override
+  @override
   Widget build(BuildContext context) {
-    var cubit=   BlocProvider.of<NewestBooksCubit>(context);
-    return BlocConsumer<NewestBooksCubit,NewestBooksState>(
-        builder: (context, state) {
-          List<BookModel> books= cubit.newestBooksList;
+    var cubit = BlocProvider.of<NewestBooksCubit>(context);
+    return BlocConsumer<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksLoadingState) {
+          // Show shimmer effect when loading
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey[600]!,
+                  highlightColor: Colors.grey[400]!,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.01.h, horizontal: 0.05.w),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 0.23.w,
+                          height: 0.18.h,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[600], // Simulated book card
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        0.05.pw,
+                         Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 0.02.h,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[600], // Simulated book card
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                              0.03.ph,
+                              Container(
+                                height: 0.02.h,
+                                width: 0.3.w,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[600], // Simulated book card
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                              0.03.ph,
+                              Row(
+                                children: [
+                                  Container(
+                                    height: 0.02.h,
+                                    width: 0.2.w,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[600], // Simulated book card
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Container(
+                                    height: 0.02.h,
+                                    width: 0.12.w,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[600], // Simulated book card
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                  ),
+                                  0.02.pw,
+                                  Container(
+                                    height: 0.02.h,
+                                    width: 0.12.w,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[600], // Simulated book card
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                  ),
+                                  0.02.pw,
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ),
+                );
+              },
+              childCount: 10, // Simulate 10 loading items
+            ),
+          );
+        }
+
+        if (state is NewestBooksSuccessState) {
+          // Show actual list of books when loaded
+          List<BookModel> books = cubit.newestBooksList;
           PrintFailure.errorMessage(message: books.length.toString());
-         return SliverList(
+          return SliverList(
             delegate: SliverChildBuilderDelegate(
                   (context, index) {
                 return Column(
                   children: [
-                    BestSellerListViewItem(bookModel: books[index],),
+                    BestSellerListViewItem(bookModel: books[index]),
                     SizedBox(
-                      height: 0.01.h,
+                      height: 0.01.h, // Space between items
                     ),
                   ],
                 );
@@ -46,11 +135,16 @@ class _BestSellerListViewState extends State<BestSellerListView> {
               childCount: books.length,
             ),
           );
-        },
+        }
 
-        listener: (context, state) {
-
-        },
+        // Show empty container or error message if no books loaded
+        return const SliverToBoxAdapter(
+          child: Center(
+            child: Text("No books available"), // Placeholder for error/empty state
+          ),
+        );
+      },
+      listener: (context, state) {},
     );
   }
 }
--- lib/feature/home/presentation/view_model/newest_books/newest_books_cubit.dart
+++ lib/feature/home/presentation/view_model/newest_books/newest_books_cubit.dart
@@ -10,13 +10,17 @@ class NewestBooksCubit extends Cubit<NewestBooksState> {
   NewestBooksCubit(this.homeRepo) : super(NewestBooksInitialState());
   List<BookModel> newestBooksList = [];
   final HomeRepo homeRepo;
+  bool loading=false;
   fetchNewestBooks() async {
+    loading=true;
     emit(NewestBooksLoadingState());
     Either<Failure, List<BookModel>> result =
         await homeRepo.fetchFeaturedBooks();
     result.fold((faliure) {
+      loading=false;
       emit(NewestBooksFaliureState(errorMessage: faliure.errorMessage));
     }, (success) {
+      loading=false;
       newestBooksList = success;
       emit(NewestBooksSuccessState());
     });
