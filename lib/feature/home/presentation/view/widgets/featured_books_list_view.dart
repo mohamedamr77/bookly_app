d4b95383a8e0220996775166340755862be3ee99
mohamedamr77 <moamr947@gmail.com>
2024-09-22 16:03:36 +0300

    remove Skeletonizer and make shimmer

--- lib/feature/home/presentation/view/widgets/featured_books_list_view.dart
+++ lib/feature/home/presentation/view/widgets/featured_books_list_view.dart
@@ -32,38 +32,37 @@ class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
 
         // Show skeleton loading when in the loading state
         if (state is FeaturedBooksLoadingState) {
-          return Skeletonizer(
-            enabled: cubit.loading, // Enable skeleton when loading
-            child: SizedBox(
-              height: 0.28.h,
-              child: Shimmer.fromColors(
-                baseColor: Colors.grey[600]!,
-                highlightColor: Colors.grey[400]!,
-                direction: ShimmerDirection.ltr, // Left to right shimmer
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: index == 0
-                          ? EdgeInsets.only(left: 0.05.w)
-                          : index == 9
-                          ? EdgeInsets.only(right: 0.07.w)
-                          : EdgeInsets.zero,
-                      child: Container(
-                        width: 0.4.w,
-                        height: 0.2.h,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[600],
-                          borderRadius: BorderRadius.circular(12),
-                        ),
+          return SizedBox(
+            height: 0.28.h,
+            child: Shimmer.fromColors(
+              enabled: cubit.loading,
+              baseColor: Colors.grey[600]!,
+              highlightColor: Colors.grey[400]!,
+              direction: ShimmerDirection.ltr, // Left to right shimmer
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: index == 0
+                        ? EdgeInsets.only(left: 0.05.w)
+                        : index == 9
+                        ? EdgeInsets.only(right: 0.07.w)
+                        : EdgeInsets.zero,
+                    child: Container(
+                      width: 0.4.w,
+                      height: 0.2.h,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => SizedBox(
-                    width: 0.04.w,
-                  ),
-                  itemCount: 10, // Simulate 10 loading skeletons
+
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(
+                  width: 0.04.w,
                 ),
+                itemCount: 10, // Simulate 10 loading skeletons
               ),
             ),
           );
