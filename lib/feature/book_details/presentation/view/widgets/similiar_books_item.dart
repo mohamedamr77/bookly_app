83a5cd9065e0f1df65459341e96bf601891c4641
mohamedamr77 <moamr947@gmail.com>
2024-09-23 18:21:01 +0300

    refecator

--- lib/feature/book_details/presentation/view/widgets/similiar_books_item.dart
+++ lib/feature/book_details/presentation/view/widgets/similiar_books_item.dart
@@ -1,8 +1,8 @@
 import 'package:booklyapp/core/utils/extentions/screen_size.dart';
 import 'package:booklyapp/feature/home/data/model/book_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/app_images.dart';
+import '../../../../home/presentation/view/widgets/shimmer_featured_books_widget/shimmer_featured_book_item_blace_holder.dart';
 
 class SimilarBooksItem extends StatelessWidget {
   const SimilarBooksItem({super.key, required this.index, required this.bookModel});
@@ -16,10 +16,34 @@ class SimilarBooksItem extends StatelessWidget {
             : index == 9
             ? EdgeInsets.only(right: 0.07.w)
             : EdgeInsets.zero,
-        child: Image(
-          image:  NetworkImage( bookModel.volumeInfo!.imageLinks!.thumbnail??"",),
-          width: 0.26.w,
-          height: 0.1.h,
-        ));
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: CachedNetworkImage(
+            fit: BoxFit.fill,
+            width: 0.26.w,
+            height: 0.1.h,
+            imageUrl: bookModel.volumeInfo!.imageLinks?.thumbnail??"",
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+            const ShimmerFeaturedBookItem(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+        ),
+
+    );
   }
 }
+
+/*
+ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+           width: 0.26.w,
+          height: 0.1.h,
+          imageUrl: bookModel.volumeInfo!.imageLinks!.thumbnail??"",
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              const ShimmerFeaturedBookItem(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+      ),
+ */
