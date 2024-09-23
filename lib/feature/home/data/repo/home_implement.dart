714ac09e5b1e2eb1f1660287c0200244aac71bd1
mohamedamr77 <moamr947@gmail.com>
2024-09-23 03:34:20 +0300

    fetch similar books

--- lib/feature/home/data/repo/home_implement.dart
+++ lib/feature/home/data/repo/home_implement.dart
@@ -76,4 +76,27 @@ class HomeImplement implements HomeRepo {
       return const Right(null); // Return success for any other connection
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var response = await apiService.get(
+          endpoint: "volumes?Filtering=free-ebooks&q=subject:programming&Sorting=relevance");
+      debugPrint(response.toString());
+      List<dynamic> booksFromApi = response["items"];
+      List<BookModel> booksList = [];
+      for (int i = 0; i < booksFromApi.length; i++) {
+        booksList.add(BookModel.fromJson(booksFromApi[i]));
+      }
+
+      return right(booksList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        debugPrint(e.toString());
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
+  }
 }
