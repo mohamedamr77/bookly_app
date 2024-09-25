44aebc552851a523d94b2f9e629666d81922ed22
mohamedamr77 <moamr947@gmail.com>
2024-09-25 17:14:16 +0300

    add refresh indicator

--- lib/feature/home/presentation/view/widgets/home_body.dart
+++ lib/feature/home/presentation/view/widgets/home_body.dart
@@ -1,6 +1,8 @@
 import 'package:booklyapp/core/utils/app_text.dart';
 import 'package:booklyapp/core/utils/extentions/screen_size.dart';
+import 'package:booklyapp/feature/home/presentation/view_model/newest_books/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/shared_widget/custom_text.dart';
 import 'appbar_home.dart';
 import 'newest_list_view.dart';
@@ -12,26 +14,37 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: CustomScrollView(
-      slivers: [
-        const SliverToBoxAdapter(child: AppbarHome()),
-        SliverToBoxAdapter(
-          child: 0.02.ph,
-        ),
-        const SliverToBoxAdapter(child: FeaturedBooksListView()),
-        SliverToBoxAdapter(
-          child: 0.04.ph,
-        ),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.only(left: 0.05.w),
-            child: const CustomText(
-              text: AppText.bestSeller,
+        child: RefreshIndicator(
+          onRefresh: () async{
+            await Future.delayed(
+              const Duration(seconds: 1),
+              () {
+               BlocProvider.of<NewestBooksCubit>(context).fetchNewestBooks();
+              },
+            );
+
+          },
+          child: CustomScrollView(
+                slivers: [
+          const SliverToBoxAdapter(child: AppbarHome()),
+          SliverToBoxAdapter(
+            child: 0.02.ph,
+          ),
+          const SliverToBoxAdapter(child: FeaturedBooksListView()),
+          SliverToBoxAdapter(
+            child: 0.04.ph,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(left: 0.05.w),
+              child: const CustomText(
+                text: AppText.bestSeller,
+              ),
             ),
           ),
-        ),
-        const BestSellerListView(),
-      ],
-    ));
+          const BestSellerListView(),
+                ],
+              ),
+        ));
   }
 }
