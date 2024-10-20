e29d2de6fa4011673b6d411bcdc4c724754245b9
mohamedamr77 <moamr947@gmail.com>
2024-10-20 22:36:29 +0300

    make cubit

--- lib/feature/saved_books/presentation/view_model/saved_books_controller/saved_books_cubit.dart
+++ lib/feature/saved_books/presentation/view_model/saved_books_controller/saved_books_cubit.dart
@@ -1,7 +1,28 @@
+import 'package:booklyapp/feature/home/data/model/book_model.dart';
 import 'package:booklyapp/feature/saved_books/presentation/view_model/saved_books_controller/saved_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SavedBooksCubit extends Cubit<SavedBooksState> {
   SavedBooksCubit() : super(SavedBooksInitialState());
+  List<BookModel> savedBooksList=[];
 
+  changeSavedBookIcon({required BookModel bookModel}){
+    if (!savedBooksList.contains(bookModel)){
+      addSavedBook(bookModel: bookModel);
+    } else {
+      removeSavedBook(bookModel: bookModel);
+    }
+  }
+
+  addSavedBook({required BookModel bookModel}){
+    bookModel.saveBook=!bookModel.saveBook;
+    savedBooksList.add(bookModel);
+    emit(SavedBooksAddState());
+  }
+
+  removeSavedBook({required BookModel bookModel}){
+    bookModel.saveBook=!bookModel.saveBook;
+    savedBooksList.remove(bookModel);
+    emit(SavedBooksRemoveState());
+  }
 }
--- lib/feature/saved_books/presentation/view_model/saved_books_controller/saved_books_state.dart
+++ lib/feature/saved_books/presentation/view_model/saved_books_controller/saved_books_state.dart
@@ -1,2 +1,6 @@
 abstract class SavedBooksState{}
 class SavedBooksInitialState extends SavedBooksState{}
+
+class SavedBooksAddState extends SavedBooksState{}
+class SavedBooksRemoveState extends SavedBooksState{}
+
